6f8b9d4a905fe3b0d3309aafba2bb0d781271ce2
Abdullah <105747181+abdullahdogankilic@users.noreply.github.com>
2023-09-05 16:49:11 +0300

    Create exercise-3.js

---
+++ exercise-3.js
@@ -0,0 +1,21 @@
+const sum = function (a, b) {
+  return a + b;
+};
+
+const subtract = function (a, b) {
+  return a - b;
+};
+
+const multiply = function (a, b) {
+  return a * b;
+};
+
+const divide = function (a, b) {
+  return a / b;
+};
+
+const log = function (value) {
+  console.log(value);
+};
+
+log(divide(subtract(multiply(sum(2, 4), sum(5, 2)), 2), 5));
