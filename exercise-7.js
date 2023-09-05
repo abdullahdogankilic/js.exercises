21d5d2dda3314482e12caee9061daadb40adccbe
Abdullah <105747181+abdullahdogankilic@users.noreply.github.com>
2023-09-05 19:03:21 +0300

    ex-7

--- exercise-7.js
+++ exercise-7.js
@@ -1,9 +1,10 @@
 function printName() {
   let helloName = "Hello John";
   function inner() {
-    return helloName;
+    setTimeout(function () {
+      console.log(helloName);
+    }, 1000);
   }
   return inner;
 }
-
-console.log(printName()());
+printName()();
