9e67ade1a09718b0bbd83bdf9d84646f763d895c
Abdullah <105747181+abdullahdogankilic@users.noreply.github.com>
2023-09-17 13:32:25 +0300

    Create exercise-32.js

---
+++ exercise-32.js
@@ -0,0 +1,14 @@
+const person = {
+  id: 1,
+  firstName: "Mario",
+  lastName: "Rossi",
+  age: 25,
+};
+const filter = {
+  id: person.id,
+  age: person.age,
+};
+
+const json = JSON.stringify(filter);
+
+console.log(json); // Should return: { id: 1, age: 25 }
