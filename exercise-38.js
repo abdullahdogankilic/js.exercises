2201265b9ee1ea5b26d0e783114a4be6450be9e3
Abdullah <105747181+abdullahdogankilic@users.noreply.github.com>
2023-09-17 15:08:31 +0300

    Create exercise-38.js

---
+++ exercise-38.js
@@ -0,0 +1,33 @@
+const isLogged = true;
+const promis = (isLogged) => {
+  return new Promise((resolve, reject) => {
+    if (isLogged) {
+      const randomNum = Math.random();
+      resolve(randomNum);
+    } else {
+      reject("False");
+    }
+  });
+};
+
+const promis1 = (num) => {
+  return new Promise((resolve, reject) => {
+    if (num > 0.5) {
+      resolve({ name: "John", age: 24 });
+    } else {
+      reject("randomV < 0.5");
+    }
+  });
+};
+
+promis(isLogged)
+  .then((message) => {
+    console.log(message);
+    return promis1(message);
+  })
+  .then((message) => {
+    console.log(message);
+  })
+  .catch((e) => {
+    console.log(e);
+  });
