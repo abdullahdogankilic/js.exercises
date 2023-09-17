569d7f540d442aac133fdf4df070b9c3cf9fdbb2
Abdullah <105747181+abdullahdogankilic@users.noreply.github.com>
2023-09-17 15:22:51 +0300

    Create exercise-40.js

---
+++ exercise-40.js
@@ -0,0 +1,20 @@
+async function tempFetch() {
+  const url = "https://jsonplaceholder.typicode.com/users";
+  let ids = [];
+
+  await fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Network response was not ok: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => {
+      console.error("There was a problem with the fetch operation:", error);
+    });
+}
+
+tempFetch();
